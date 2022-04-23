53f3d1623dfe3d4398050a647ceda65729e4dd55
yuy040 <yuy040@ucsd.edu>
2022-04-23 16:07:21 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,14 +12,14 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int exclamation = markdown.indexOf("!", currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (exclamation != openBracket - 1) {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (openParen == -1 || closeParen == -1) {
+                break;
             }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
 

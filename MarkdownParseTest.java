39c165817cca92b2da980eab4b85fe68f9b45d91
yuy040 <yuy040@ucsd.edu>
2022-05-26 17:37:59 -0700

    SkillDemo

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,22 +8,10 @@ import java.util.*;
 
 public class MarkdownParseTest {
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
-
-    // new test
-    @Test
     public void TestFile() throws IOException {
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("snippet3.md");
         String content = Files.readString(fileName);
-        assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(content));
-    }
-
-    @Test
-    public void Test() throws IOException {
-        Path fileName = Path.of("test.md");
-        String content = Files.readString(fileName);
-        assertEquals(List.of("https://www.google.com"), MarkdownParse.getLinks(content));
+        assertEquals(List.of("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"), 
+        MarkdownParse.getLinks(content));
     }
 }
